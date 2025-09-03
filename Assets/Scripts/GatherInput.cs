696849d96bda686fc1ae081cf1f39227be6d62f8
Marq91 <anibal.marquez.91@gmail.com>
2025-09-03 00:37:25 -0400

    Added Sprite Jumps, jump state, player wall physics2d, material and headers

--- Assets/Scripts/GatherInput.cs
+++ Assets/Scripts/GatherInput.cs
@@ -5,8 +5,11 @@ public class GatherInput : MonoBehaviour
 {
     private Controls controls;
     [SerializeField] private float _valueX;
-
     public float ValueX { get => _valueX; }
+
+    [SerializeField] private bool _isJumping;
+    public bool IsJumping { get => _isJumping; set => _isJumping = value; }
+
 
     private void Awake()
     {
@@ -18,6 +21,8 @@ public class GatherInput : MonoBehaviour
         // Asociar Fases Performed, Canceled, Waiting or Started
         controls.Player.Move.performed += StartMove;
         controls.Player.Move.canceled += StopMove;
+        controls.Player.Jump.performed += StartJump;
+        controls.Player.Jump.canceled += StopJump;
         controls.Player.Enable();
     }
 
@@ -34,10 +39,22 @@ public class GatherInput : MonoBehaviour
         Debug.Log("Value X stoped: " + _valueX);
     }
 
+    private void StartJump(InputAction.CallbackContext context)
+    {
+        _isJumping = true;
+    }
+
+    private void StopJump(InputAction.CallbackContext context)
+    {
+        _isJumping = false;
+    }
+
     private void OnDisable()
     {
         controls.Player.Move.performed -= StartMove;
         controls.Player.Move.canceled -= StopMove;
+        controls.Player.Jump.performed -= StartJump;
+        controls.Player.Jump.canceled -= StopJump;
         controls.Player.Disable();
     }
 }
